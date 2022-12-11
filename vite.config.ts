2612c46d310d0a38d489052e5e2f100d9e162aa0
amangeldi0 <amanimanbekov12@gmail.com>
2022-12-11 15:41:52 +0600

    install pkg and change some config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils/api': path.resolve(__dirname, './src/utils/api'),
+      '@assets': path.resolve(__dirname, './src/assets')
+    }
+  }
 });
