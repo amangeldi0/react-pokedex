b78a6690b3bc32c64d1c8bb521c652829a9d6956
amangeldi0 <amanimanbekov12@gmail.com>
2022-12-18 11:35:33 +0600

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Header } from '@components';
 import {PokemonsPage} from "@pages";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ROUTES } from "./utils/constants/ROUTES";
 
 
 export const App: React.FC = () => {
-  return <div className='App'>
-    <Header />
-    <PokemonsPage/>
-  </div>;
+  return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+
+            <Route path={ROUTES.POKEMONS} element={<PokemonsPage />}/>
+            <Route path='*' element={<Navigate to={ROUTES.POKEMONS} />}/>
+        </Routes>
+      </BrowserRouter>
+  );
 };
